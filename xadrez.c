b2754dc71d6fb24d03e231e2990998d0d7c6d2f8
gbycode <gabrielryanrcarvalho24@gmail.com>
2025-10-28 01:27:52 +0000

    xadrez nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,9 +5,11 @@ int main() {
     int i; //controle do for da Torre
     int casasBispo; //controle do while do Bispo
     int casasRainha; //controle do do-while da Rainha
+    int passoCavalo; //controle do loop interno Cavalo
+    int etapasCavalo; //controle do loop externo Cavalo
 
 
-    printf("\n=======Xadrez nível novato=======\n");
+    printf("\n=======Xadrez nível Aventureiro=======\n");
     //MOVIMENTAÇÃO DA TORRE - 5 casas para direita
 
     printf("\nMovimento da torre: \n");
@@ -33,6 +35,22 @@ int main() {
         printf("Esquerda\n");
         casasRainha++;
     } while (casasRainha < 8);
+
+    printf("\n");
+
+    //MOVIMENTAÇÃO DO CAVALO - 2 casas para baixo + 1 casa para esquerda
+    //loops aninhados
+    printf("Movimento do cavalo: \n");
+    etapasCavalo = 1; //controla o número de movimentos "L"
+    
+    for (etapasCavalo = 1; etapasCavalo <= 1; etapasCavalo++) { //for externo
+        passoCavalo = 1;
+        while (passoCavalo <= 2){ //move 2 casas para baixo
+            printf("Baixo\n");
+            passoCavalo++;
+        }
+        printf("Esquerda\n"); //move 1 casa para esquerda
+    }
 
     return 0;
 }
